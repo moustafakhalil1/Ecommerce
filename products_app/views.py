25556e90c08b036accc83a79d8cb62fc8c2d26f8
Moustafa4180527 <103715883+moustafakhalil1@users.noreply.github.com>
2023-10-08 00:24:44 +0300

    Update views.py

--- products_app/views.py
+++ products_app/views.py
@@ -5,24 +5,18 @@ from django.core.paginator import Paginator
 from .models import product,cart
 from django.contrib.auth.decorators import login_required
 from .models import orderItem,order
+from user.utility import CookieCart ,CartData
 # Create your views here.
 
-@login_required(login_url='user-login')
+
 def index(request):
     products=product.objects.all()
     Paginatorr=Paginator(products,4) 
     page = request.GET.get("page")
     products=Paginatorr.get_page(page)
-
-    if request.user.is_authenticated:
-       customer=request.user.custommer
-       ord,created=order.objects.get_or_create(customer=customer,complete=False)
-       #then  we need to get all the items in that order by 
-
-       cartitemCoount=ord.get_Total_items
-    else:
-       ordere={'get_Total_items':0,'get_Total_order_price':0,'shipping':False}
-       cartitemCoount=ordere['get_Total_items']
+    
+    CookieData=CartData(request)
+    cartitemCoount=CookieData['cartitemCoount']
 
     context={
         'products':products,
@@ -32,31 +26,27 @@ def index(request):
     }
     return render(request,'products/index.html',context)
 
-@login_required(login_url='user-login')
+
 def single_product(request,slug):
     from .models import cart  
     product_id=product.objects.get(PRDslug=slug)
     accessory=productimage.objects.filter(PRDIproduct=product_id)
-   
-    if request.method=="POST":
-       q=request.POST.get('quantity')
-       p=request.POST.get(slug)
-       obj=cart(Quantity=q,product=product_id)
-       obj.save()
-      
+
+    CookieData=CartData(request)
+    cartitemCoount=CookieData['cartitemCoount']
+    ord=CookieData['ord']
+    items=CookieData['items']
+  
     context={
+        'items':items,
+        'order':ord,
+        'cartitemCoount':cartitemCoount,
         'singlproduct':product_id,
         'ac':accessory,
     }
     return render(request,'products/single_product.html',context)    
 
-def search(request):
-    q=request.GET['query']
-    products=product.objects.filter(PRDName__icontains=q)
-    context={
-        'products':products,     
-    }
-    return render(request,'products/search.html',context)
+
 
 
 
